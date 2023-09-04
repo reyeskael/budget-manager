2fae933f510ac45e7dfd433b4de9fbd185c832d3
Michael Reyes <michaelreyes0202@gmail.com>
2023-09-04 18:26:59 +0800

    Add show password logic

--- client/src/page/LoginPage/LoginPage.tsx
+++ client/src/page/LoginPage/LoginPage.tsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 import './LoginPage.css';
-import { Box, Button, Container, TextField } from '@mui/material';
+import { Box, Button, Container, IconButton, InputAdornment, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { postRequest } from '../../utils/apiHelper';
 import { useDispatch } from 'react-redux';
 import { addLogin } from '../../action/loginAction';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const LoginPage: React.FC = () => {
 	const [ username, setUsername ] = useState("");
 	const [ password, setPassword ] = useState("");
+	const [ isShowPassword, setIsShowPassword ] = useState(false);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
+	const toggleShowPassword = () => {
+		setIsShowPassword(!isShowPassword);
+	};
 
 	async function login() {
 		try {
@@ -39,12 +45,21 @@ const LoginPage: React.FC = () => {
 					/>
 					<TextField
 						label="Password"
-						type="password"
+						type={isShowPassword ? "text" : "password"}
 						variant="outlined"
 						fullWidth
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
 						margin="normal"
+						InputProps={{
+						  endAdornment: (
+							<InputAdornment position="end">
+							  <IconButton onClick={toggleShowPassword}>
+								{isShowPassword ? <VisibilityOff /> : <Visibility />}
+							  </IconButton>
+							</InputAdornment>
+						  ),
+						}}
 					/>
 					<Button
 						variant="contained"
