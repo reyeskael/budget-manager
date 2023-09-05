29dcf01eb0177819f3ba03b74f665e150cbaa638
Michael Reyes <michaelreyes0202@gmail.com>
2023-09-05 22:50:16 +0800

    Add new transaction form

--- client/src/page/SavingsDetailsPage/SavingsDetailsPage.tsx
+++ client/src/page/SavingsDetailsPage/SavingsDetailsPage.tsx
@@ -1,16 +1,92 @@
 import './SavingsDetailsPage.css';
-import { Container } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../reducer/rootReducer';
-import { pageContainer } from '../../utils/cosmeticsHelper';
+import { footerButton, pageContainer } from '../../utils/cosmeticsHelper';
 import SavingsDetailsCard from './SavingsDetailsCard/SavingsDetailsCard';
+import { useState } from 'react';
+import FormWindow, { FormWindowItemProps, FormWindowItemType, FormWindowSubmitEvent } from '../../component/FormWindow/FormWindow';
 
 const SavingsDetailsPage = () => {
 	const savingsState = useSelector((state: RootState) => state?.savingsReducer);
-	console.log(savingsState);
+	const [ isAddNewTransactionOpen, setIsAddNewTransactionOpen ] = useState(false);
+	const newTransactionFormItems: FormWindowItemProps[] = [
+		{
+			type: FormWindowItemType.DROPDOWN,
+			label: "Type",
+			key: "type",
+			required: true,
+			options: [
+				{
+					label: "Savings",
+					value: "savings"
+				},
+				{
+					label: "Withdrawal",
+					value: "withdrawal"
+				}
+			]
+		},
+		{
+			type: FormWindowItemType.TEXTFIELD,
+			inputType: "number",
+			label: "Amount",
+			key: "amount",
+			required: true
+		},
+		{
+			type: FormWindowItemType.DATEPICKER,
+			label: "Date",
+			key: "date",
+			required: true
+		},
+		{
+			type: FormWindowItemType.TEXTFIELD,
+			label: "Note",
+			key: "note"
+		}
+	];
+	
+	function onAddNewTransactionClick(e: any) {
+		console.log(e);
+		setIsAddNewTransactionOpen(true);
+	}
+
+	function onCloseAddNewTransactionClick(e: any) {
+		console.log(e);
+		setIsAddNewTransactionOpen(false);
+	}
+
+	function onAddNewTransactionSubmit(e: FormWindowSubmitEvent) {
+		const type = e.data.type?.toString() || "";
+		const date = e.data.date?.toString() || "";
+		const amount = e.data.amount?.toString() || "";
+		console.log(e);
+		// addBudgetCategory(name);
+		setIsAddNewTransactionOpen(false);
+	}
+
 	return (
 		<Container className="pageContainerWithHeader" sx={pageContainer}>
 			<SavingsDetailsCard data={savingsState}/>
+			{
+				isAddNewTransactionOpen ?
+				<FormWindow 
+					title="Add New Transaction"
+					items={newTransactionFormItems}
+					onSubmit={onAddNewTransactionSubmit}
+					onCancelClick={onCloseAddNewTransactionClick}
+				/> :
+				null
+			}
+			<Button
+				sx = {footerButton}
+				onClick={onAddNewTransactionClick}
+				variant="contained"
+				color="secondary"
+			>
+				Add New Transaction
+			</Button>
 		</Container>
 	);
 }
